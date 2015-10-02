6f093a18992b4908fd1a775e3da9aae6a1aa9325
Leandro Glossman <lglossman@gmail.com>
2015-10-02 08:50:28 -0300

    User model

---
+++ app/src/main/java/com/nanospark/gard/model/user/ControlSchedule.java
@@ -0,0 +1,12 @@
+package com.nanospark.gard.model.user;
+
+/**
+ * Created by Leandro on 1/10/2015.
+ */
+public class ControlSchedule {
+
+    public enum Repeat {
+        DAILY, WEEKLY, MONTHLY, YEARLY, NONE
+    }
+
+}
--- app/src/main/java/com/nanospark/gard/model/user/User.java
+++ app/src/main/java/com/nanospark/gard/model/user/User.java
@@ -1,7 +1,85 @@
 package com.nanospark.gard.model.user;
 
+import java.util.List;
+
+import mobi.tattu.utils.StringUtils;
+
 /**
  * Created by Leandro on 1/10/2015.
  */
 public class User {
+
+    private String name;
+    private String phone;
+    private String password;
+    private Notify notify;
+    private List<ControlSchedule> schedules;
+
+    public User() {
+    }
+
+    public User(String name, String phone, String password, Notify notify) {
+        this.name = name;
+        this.phone = phone;
+        this.password = password;
+        this.notify = notify;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Notify getNotify() {
+        return notify;
+    }
+    public void setNotify(Notify notify) {
+        this.notify = notify;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public List<ControlSchedule> getSchedules() {
+        return schedules;
+    }
+    public void setSchedules(List<ControlSchedule> schedules) {
+        this.schedules = schedules;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    public boolean isPasswordRequired() {
+        return StringUtils.isNotBlank(password);
+    }
+
+    public boolean isNotificationEnabled() {
+        return notify != null && !notify.equals(Notify.NONE);
+    }
+
+    public enum Notify {
+        OPEN, CLOSE, ALL, NONE
+    }
+
 }
