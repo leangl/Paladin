cb6be6ee2be73659dca4d056c8074ff5e820236c
cristian figueroa <cristian.alberto.figueroa@gmail.com>
2015-10-16 11:47:30 -0300

    Primera version de la pantalla User

--- app/src/main/java/com/nanospark/gard/ui/fragments/UsersFragment.java
+++ app/src/main/java/com/nanospark/gard/ui/fragments/UsersFragment.java
@@ -3,6 +3,7 @@ package com.nanospark.gard.ui.fragments;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -29,8 +30,20 @@ public class UsersFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_users, container, false);
+        View view = inflater.inflate(R.layout.fragment_users, container, false);
+        GridLayout  gridLayout = (GridLayout) view.findViewById(R.id.gridlayout);
+        gridLayout.setUseDefaultMargins(true);
+        int size = 2;
+        for (int i = 0 ; i < size ; i++){
+            View userView1 = inflater.inflate(R.layout.user_layout,null,false);
+            GridLayout.LayoutParams  layoutParams = new GridLayout.LayoutParams();
+            layoutParams.rowSpec = GridLayout.spec(1,Float.valueOf("1.0"));
+            layoutParams.columnSpec = GridLayout.spec(GridLayout.UNDEFINED,Float.valueOf("1.0"));
+            userView1.setLayoutParams(layoutParams);
+            gridLayout.addView(userView1);
+
+        }
+        return view;
     }
 
 
