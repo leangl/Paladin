1b0b0da6811183a21715c999b90169206dac74f2
Leandro Glossman <lglossman@gmail.com>
2015-12-06 18:15:30 -0300

    Fix sms event count

--- app/src/main/java/com/nanospark/gard/model/user/UserManager.java
+++ app/src/main/java/com/nanospark/gard/model/user/UserManager.java
@@ -83,7 +83,7 @@ public class UserManager {
     @Subscribe
     public void on(CommandProcessed event) {
         if (event.command != null && event.command.user != null && event.command.user.getSchedule() != null
-                && Limit.EVENTS.equals(event.command.user.getSchedule())) {
+                && Limit.EVENTS.equals(event.command.user.getSchedule().getLimit())) {
             ToastManager.show("Command processed");
             event.command.user.getSchedule().incrementEvents();
             update(event.command.user);
